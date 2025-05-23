9bd0f38fdb507edf7aba9c5576c96d07be35e592
lunolunoluno <noellucien2001@gmail.com>
2025-05-23 14:41:54 +0200

    Function assert_pgn_well_formatted not needed as StringExporter also check if pgn is well formatted

--- modules/datautils.py
+++ modules/datautils.py
@@ -1,17 +1,11 @@
 import re
 from typing import List
 
-def assert_pgn_well_formatted(pgn: str)->None:
-    # Work in progress
-    pass
-
 def has_game_comments(pgn: str)->bool:
-    self.assert_pgn_well_formatted(pgn)
     comment_pattern = r'\{.*?\}'
     return re.search(comment_pattern, pgn) is not None
 
 def get_all_comments_after_error(pgn: str)->List[str]:
-    assert_pgn_well_formatted(pgn)
     pgn_cleaned = re.sub(r'[\r\n]+', '', pgn) # remove all return characters
     pattern = r'\$\d[ \t]*\{(.*?)}' # get all comments after $ followed by a number (this is because python-chess replace the NAGs with those for some reason)
     return re.findall(pattern, pgn_cleaned)
