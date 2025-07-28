5d98160d569ce92285aaf8b985d173bcdd8943b7
lunolunoluno <noellucien2001@gmail.com>
2025-07-28 14:31:15 +0200

    added more data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import chess.pgn
+import pandas as pd
 
 from controller import Controller
 from modules.utils import Debug, LLM
@@ -16,3 +17,7 @@ if __name__ == "__main__":
     dataset_path = os.path.join(".", "data", "filtered_merged_20250708_134716.csv")
     ctrl.train_model(dataset_path, ["context", "moves"], "reformulated")
 
+
+    df = pd.read_csv(dataset_path)
+    num_of_rows = df.shape[0]
+    print("Number of data :", num_of_rows)
