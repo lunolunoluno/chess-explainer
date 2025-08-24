f8bda7e128b628ed3350e2e8867e7f50de465b10
lunolunoluno <noellucien2001@gmail.com>
2025-08-24 10:30:44 +0200

    setup new training function

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import os
 
 import pandas as pd
-from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # These 2 lines prevent pytorch from trying to use Triton
 os.environ['TORCH_COMPILE_DISABLE'] = '1'
 os.environ['TORCHDYNAMO_DISABLE'] = '1'
 
-import re
 from controller import Controller
 from modules.utils import Debug, LLM
 from sklearn.model_selection import train_test_split
 
-
 if __name__ == "__main__":
-
     dbg = Debug(debug=True)
     dbg.print("Debug: On")
 
@@ -23,12 +19,14 @@ if __name__ == "__main__":
     dataset = pd.read_csv(dataset_path)
     dataset_train, dataset_eval = train_test_split(dataset, test_size=0.2, shuffle=True)
 
-    # This will be used as temporary models but in the final product the trained model will be used
-    model_id = "google/gemma-3-1b-it"
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForCausalLM.from_pretrained(model_id)
+    input_columns = ["moves", "engine_eval", "engine_best_line", "engine_best_alternative"]
+    input_target = "comment"  # TODO: later replace that with reformulated
 
-    input_columns = ["moves", "context"] # TODO: replace with the correct list
-    input_target = "reformulated"
+    checkpoint_name = ctrl.train_model(
+        dataset_path,
+        input_columns,
+        input_target
+    )
+    model, tokenizer = ctrl.load_model_and_tokenizer_from_checkpoint(checkpoint_name)
 
     res = ctrl.evaluate_model(model, tokenizer, dataset_eval, input_columns, input_target, 2, True, True)
